c41298dda48fe287acc2329487cfc12dbd490253
thugnificentve <venantm99@gmail.com>
2021-06-17 10:33:29 -0700

    updates

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,9 @@
-#!/bin/bash
-int puts("\\"Programming is like building a multilingual puzzle)
+#include <stdio.h>
+
+
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+
+return (0);
+}
