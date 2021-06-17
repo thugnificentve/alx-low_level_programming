f5c3d30cbe6f05aeaffbf52a612cd15c15ab3b0b
thugnificentve <venantm99@gmail.com>
2021-06-17 10:38:02 -0700

    updates

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
-
+/**
+ * main -displays the phrase "Programming is
+ *,like building a multilingual puzzle
+ * Return: 0 if exited properly, non-zero otherwise
+ */
 
 int main(void)
 {
